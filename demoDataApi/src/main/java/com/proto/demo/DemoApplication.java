65296bee0cff60e0734aa5918a9e957aad536795
Yukiaki Saito <1617687@TCS.com>
2021-11-16 19:17:55 +0900

    upd

--- demoDataApi/src/main/java/com/proto/demo/DemoApplication.java
+++ demoDataApi/src/main/java/com/proto/demo/DemoApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
 @SpringBootApplication
 public class DemoApplication {
