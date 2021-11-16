a42933ebb8a5d14bd77a3917625dfce6c5e1fc65
Yukiaki Saito <1617687@TCS.com>
2021-11-16 19:07:32 +0900

    add

---
+++ demoDataApi/src/main/java/com/proto/demo/DemoApplication.java
@@ -0,0 +1,60 @@
+package com.proto.demo;
+
+import java.util.Map;
+import java.util.function.Function;
+
+import com.amazonaws.services.rdsdata.AWSRDSData;
+import com.amazonaws.services.rdsdata.AWSRDSDataClient;
+import com.amazonaws.services.rdsdata.model.ExecuteStatementRequest;
+import com.amazonaws.services.rdsdata.model.ExecuteStatementResult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+    public Function<Message<Map<String, Object>>, Message<Map<String, Object>>> handler() {
+        return request -> handleRequest(request);
+    }
+
+	public static final String RESOURCE_ARN = "arn:aws:rds:us-east-2:079862682662:cluster:pointsystem";
+    public static final String SECRET_ARN = "arn:aws:secretsmanager:us-east-2:079862682662:secret:point-mysql-B3EQyV";
+          
+    public Message<Map<String, Object>> handleRequest(Message<Map<String, Object>> input) {
+
+		Map<String, Object> bodyMap = (Map<String, Object>) ((Map<String, Object>) input.getPayload().get("body"));
+		int customerId = (Integer) bodyMap.get("CustomerId");
+        int pointValue = (Integer) bodyMap.get("Points");
+
+		Map<String, Object> authMap = (Map<String, Object>) ((Map<String, Object>) input.getPayload().get("requestContext")).get("authorizer");
+		Map<String, Object> userMap = (Map<String, Object>) authMap.get("claims");
+		String username = (String)userMap.get("cognito:username");
+
+		String sql = "INSERT INTO history (customer_id, point, type, properties, updated_by_user, updated_datetime) VALUES (%1$d, %2$d, 'Java', 'javaTest', '%3$s', current_timestamp)";
+        sql = String.format(sql, customerId, pointValue, username);
+        System.out.println(sql);
+
+        AWSRDSData rdsData = AWSRDSDataClient.builder().build();
+        ExecuteStatementRequest rdsRequest = new ExecuteStatementRequest()
+                .withResourceArn(RESOURCE_ARN)
+                .withSecretArn(SECRET_ARN)
+                .withDatabase("pointsystem")
+                .withSql(sql);
+//        RdsDataClient client = RdsDataClient.builder().build();
+
+        HashMap<String, Object> returnMap = new HashMap<String, Object>();
+        returnMap.put("statusCode", 201);
+        returnMap.put("body", "data inserted successfully!");
+        Message<Map<String, Object>> message = MessageBuilder.withPayload((Map<String, Object>)returnMap).setHeader("Access-Control-Allow-Origin", "*").build();
+        return message;
+
+	}
+}
